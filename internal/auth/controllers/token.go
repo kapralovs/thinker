d495dfca8f45835a78afb140aa1c690c5c898597
Sergey Kapralov <ser-kapralov@yandex.ru>
2022-11-21 15:44:37 +0300

    WIP

--- internal/auth/controllers/token.go
+++ internal/auth/controllers/token.go
@@ -1,6 +1,12 @@
 package controllers
 
-import "github.com/golang-jwt/jwt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
 
 type CustomClaimsExample struct {
 	*jwt.StandardClaims
@@ -12,10 +18,28 @@ type TokenInfo struct {
 	Token string `json:"token,omitempty"`
 }
 
-func generateToken() (string, error) {
-	return token, nil
+func generateToken(a *AuthInfo, sString string) (string, error) {
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, CustomClaimsExample{
+		&jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Minute * 1).Unix(),
+		},
+		"access",
+		a,
+	})
+
+	return t.SignedString([]byte(sString))
 }
 
-func parseToken() error {
+func parseToken(tokenString string) error {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaimsExample{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SIGN_STRING")), nil
+	})
+	if claims, ok := token.Claims.(*CustomClaimsExample); ok && token.Valid {
+		fmt.Printf("%v %v\n", claims.Login, claims.ExpiresAt)
+		fmt.Printf("%v %v\n", claims.Password, claims.ExpiresAt)
+	} else {
+		return err
+	}
+
 	return nil
 }
