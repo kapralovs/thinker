3c61f477dd97f521ac33c28786942f07aa790895
Sergey Kapralov <ser-kapralov@yandex.ru>
2023-06-23 12:43:27 +0300

    Updated scripts

--- scripts/create_note.sh
+++ scripts/create_note.sh
@@ -1,5 +1,5 @@
 curl -X POST localhost:8080/note/create \
--H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjM2MDAwMDAwMDAwMDAsInVzZXIiOnsidXNlcm5hbWUiOiJ1c2VyMSIsInBhc3N3b3JkIjoicGFzcyJ9fQ.ZTIkUeAglpVrQTqZDXqBXLrCKHbEehjOjSyPBaZRADo' \
+-H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjM2MDAwMDAwMDAwMDAsInVzZXIiOnsidXNlcm5hbWUiOiJ1c2VyMSIsInBhc3N3b3JkIjoicGFzcyJ9fQ.rR4vc0ulbKwmSzokNPNrCNd12tGAN5SaKWwCOZ3x5tc' \
 -H 'Content-Type: application/json' \
 -d '
 {
