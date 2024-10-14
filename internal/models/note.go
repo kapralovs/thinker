0d28bec6bc3e63079f47c88fbb43a7052ee6b09b
kapralovs <ser-kapralov@yandex.ru>
2024-10-15 00:13:12 +0300

    Update models

--- internal/models/note.go
+++ internal/models/note.go
@@ -1,9 +1,9 @@
 package models
 
 type Note struct {
-	ID        int64    `json:"id"`
-	CreatedBy string   `json:"created_by"`
-	Title     string   `json:"title"`
-	Text      string   `json:"text"`
-	Tags      []string `json:"tags"`
+	ID        int64  `json:"id"`
+	CreatedBy string `json:"created_by"`
+	Title     string `json:"title"`
+	Text      string `json:"text"`
+	// Tags      []string `json:"tags"`
 }
