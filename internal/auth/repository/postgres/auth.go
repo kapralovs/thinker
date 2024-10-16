7b6d6de96e867ae43cfe7dd63738569e00515a7e
kapralovs <ser-kapralov@yandex.ru>
2024-10-16 23:54:55 +0300

    Fix auth repository

--- internal/auth/repository/postgres/auth.go
+++ internal/auth/repository/postgres/auth.go
@@ -11,6 +11,24 @@ type UserRepo struct {
 	db *pgxpool.Pool
 }
 
+const (
+	dbCreateUser = `
+	INSERT INTO users (
+		name,
+		username,
+		password,
+		current_token
+	) VALUES ($1, $2, $3, $4) RETURNING id;`
+
+	dbGetUser = `
+	SELECT 
+		*
+	FROM users 
+	WHERE username = $1 
+	AND password = $2;
+	`
+)
+
 func NewAuthRepo(db *pgxpool.Pool) *UserRepo {
 	return &UserRepo{
 		db: db,
@@ -18,9 +36,11 @@ func NewAuthRepo(db *pgxpool.Pool) *UserRepo {
 }
 
 func (r *UserRepo) CreateUser(user *models.User) error {
-	if err := r.db.QueryRow(context.Background(), "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id",
+	if err := r.db.QueryRow(context.Background(), dbCreateUser,
+		user.Username,
 		user.Username,
 		user.Password,
+		user.CurrentToken,
 	).Scan(&user.ID); err != nil {
 		return err
 	}
@@ -30,7 +50,8 @@ func (r *UserRepo) CreateUser(user *models.User) error {
 
 func (r *UserRepo) GetUser(username, password string) (*models.User, error) {
 	user := new(models.User)
-	if err := r.db.QueryRow(context.Background(), "SELECT * FROM users WHERE username = $1 AND password = $2",
+
+	if err := r.db.QueryRow(context.Background(), dbGetUser,
 		user.Username,
 		user.Password,
 	).Scan(&user.ID); err != nil {
